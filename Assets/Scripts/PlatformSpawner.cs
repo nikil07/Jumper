04097389847f45d0043f423f8bdef5503597c9c9
Nikhil Kumar S <nikilkumar0810@gmail.com>
2021-08-16 22:43:07 +0530

    Platform Generator added. Needs tuning, but its up.

--- Assets/Scripts/PlatformSpawner.cs
+++ Assets/Scripts/PlatformSpawner.cs
@@ -5,18 +5,38 @@ using UnityEngine;
 public class PlatformSpawner : MonoBehaviour
 {
     [SerializeField] GameObject platformPrefab;
+    [SerializeField] int platformSpawnHeight;
 
     private Player player;
+    private float elapsedTime = 0;
+    private Vector3 spawnPoint, previousSpawnPoint; 
 
     // Start is called before the first frame update
     void Start()
     {
         player = FindObjectOfType<Player>();
+        previousSpawnPoint = new Vector3(getRandomX(), -12, 1);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        elapsedTime = Time.fixedTime;
+        if (elapsedTime % 2 == 0) {
+            print("Spawn a platform");
+            spawnPoint = new Vector3(getRandomX(), getRandomY((int)previousSpawnPoint.y),1);
+            print(spawnPoint);
+            GameObject platform = Instantiate(platformPrefab, spawnPoint, Quaternion.identity,transform);
+            previousSpawnPoint = platform.transform.position;
+        }
+    }
+
+    private float getRandomX() {
+        return Random.Range(-7, 9);
+    }
+
+    private float getRandomY(int previousY)
+    {
+        return Random.Range(previousY + platformSpawnHeight, previousY + platformSpawnHeight + 3);
     }
 }
